b84a420b379919c54c458bda44977cc188e8f7b5
Jörn Maske <j.maske@otterbach.de>
2018-01-03 11:00:09 +0100

    Fixed API routes from GET to POST

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/addGuest', 'GuestController@addGuest');
-Route::get('/removeGuest', 'GuestController@addGuest');
-Route::get('/getGuestsStatus', 'GuestController@getGuestsStatus');
-Route::get('/getGuestsList', 'GuestController@getGuestsList');
-Route::get('/getUndecidedGuestsList', 'GuestController@getUndecidedGuestsList');
-Route::get('/getConfirmedGuestsList', 'GuestController@getConfirmedGuestsList');
-Route::get('/getUnableGuestsList', 'GuestController@getUnableGuestsList');
-Route::get('/confirmGuestStatus', 'GuestController@confirmGuestStatus');
-Route::get('/calloffGuestStatus', 'GuestController@calloffGuestStatus');
+Route::POST('/addGuest', 'GuestController@addGuest');
+Route::POST('/removeGuest', 'GuestController@addGuest');
+Route::POST('/getGuestsStatus', 'GuestController@getGuestsStatus');
+Route::POST('/getGuestsList', 'GuestController@getGuestsList');
+Route::POST('/getUndecidedGuestsList', 'GuestController@getUndecidedGuestsList');
+Route::POST('/getConfirmedGuestsList', 'GuestController@getConfirmedGuestsList');
+Route::POST('/getUnableGuestsList', 'GuestController@getUnableGuestsList');
+Route::POST('/confirmGuestStatus', 'GuestController@confirmGuestStatus');
+Route::POST('/calloffGuestStatus', 'GuestController@calloffGuestStatus');
