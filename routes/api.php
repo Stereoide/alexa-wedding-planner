43a666c3965a376ac83295f13cb7479cea628667
Jörn Maske <j.maske@otterbach.de>
2018-01-03 10:18:20 +0100

    Dummy "add guest" functionality

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/addGuest', 'GuestController@addGuest');
-Route::get('/removeGuest', 'GuestController@addGuest');
-Route::get('/getGuestsStatus', 'GuestController@getGuestsStatus');
-Route::get('/getGuestsList', 'GuestController@getGuestsList');
-Route::get('/getUndecidedGuestsList', 'GuestController@getUndecidedGuestsList');
-Route::get('/getConfirmedGuestsList', 'GuestController@getConfirmedGuestsList');
-Route::get('/getUnableGuestsList', 'GuestController@getUnableGuestsList');
-Route::get('/confirmGuestStatus', 'GuestController@confirmGuestStatus');
-Route::get('/calloffGuestStatus', 'GuestController@calloffGuestStatus');
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('/addGuest', 'GuestController@addGuest');
+    Route::get('/removeGuest', 'GuestController@addGuest');
+    Route::get('/getGuestsStatus', 'GuestController@getGuestsStatus');
+    Route::get('/getGuestsList', 'GuestController@getGuestsList');
+    Route::get('/getUndecidedGuestsList', 'GuestController@getUndecidedGuestsList');
+    Route::get('/getConfirmedGuestsList', 'GuestController@getConfirmedGuestsList');
+    Route::get('/getUnableGuestsList', 'GuestController@getUnableGuestsList');
+    Route::get('/confirmGuestStatus', 'GuestController@confirmGuestStatus');
+    Route::get('/calloffGuestStatus', 'GuestController@calloffGuestStatus');
+});
