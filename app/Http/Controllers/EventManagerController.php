6773f51e08d32bc90556bff64a74e91a256a10f4
Jörn Maske <j.maske@otterbach.de>
2018-01-03 16:11:18 +0100

    Trial & Error

--- app/Http/Controllers/EventManagerController.php
+++ app/Http/Controllers/EventManagerController.php
@@ -51,9 +51,43 @@ class EventManagerController extends Controller
                     $guestsUndecided = Guest::undecided()->get();
                     $guestsUnable = Guest::unable()->get();
 
-                    $responseText = ($guestsConfirmed->isEmpty() ? 'Es haben noch keine Gäste zugesagt' : $guestsConfirmed->count() . ' Gäste haben zugesagt');
-                    $responseText .= ', ' . ($guestsUnable->isEmpty() ? 'es haben noch keine Gäste abgesagt' : $guestsUnable->count() . ' Gäste haben abgesagt');
-                    $responseText .= ' und ' . ($guestsUndecided->isEmpty() ? 'es sind keine Anmeldungen mehr offen' : $guestsConfirmed->count() . ' Gäste haben sich noch nicht entschieden.');
+                    if ($guestsConfirmed->isEmpty() && $guestsUndecided->isEmpty() && $guestsUnable->isEmpty()) {
+                        $responseText = 'Es liegen noch keine Anmeldungen vor.';
+                    } else {
+                        $responseText = '';
+
+                        if ($guestsConfirmed->isEmpty()) {
+                            $responseText .= 'Es haben noch keine Gäste zugesagt';
+                        } else {
+                            if ($guestsConfirmed->count() == 1) {
+                                $responseText .= 'Ein Gast hat zugesagt';
+                            } else {
+                                $responseText .= $guestsConfirmed->count() . ' Gäste haben zugesagt';
+                            }
+                        }
+
+                        $responseText .= ', ';
+                        if ($guestsUnable->isEmpty()) {
+                            $responseText .= 'es haben noch keine Gäste abgesagt';
+                        } else {
+                            if ($guestsUnable->count() == 1) {
+                                $responseText .= 'ein Gast hat abgesagt';
+                            } else {
+                                $responseText .= $guestsUnable->count() . ' Gäste haben abgesagt';
+                            }
+                        }
+
+                        $responseText .= ' und ';
+                        if ($guestsUndecided->isEmpty()) {
+                            $responseText .= 'es sind keine Anmeldungen mehr offen';
+                        } else {
+                            if ($guestsUndecided->count() == 1) {
+                                $responseText .= 'ein Gast hat sich noch nicht entschieden.';
+                            } else {
+                                $responseText .= $guestsUndecided->count() . ' Gäste haben sich noch nicht entschieden.';
+                            }
+                        }
+                    }
 
                     $response->respond($responseText);
 
