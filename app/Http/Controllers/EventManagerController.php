7e0578359c5112f793c6bc6eed38d99820bd8ae1
Jörn Maske <j.maske@otterbach.de>
2018-01-03 11:36:51 +0100

    Branched Intent responses

--- app/Http/Controllers/EventManagerController.php
+++ app/Http/Controllers/EventManagerController.php
@@ -4,19 +4,90 @@ namespace App\Http\Controllers;
 
 use Alexa\Request\IntentRequest;
 use Illuminate\Http\Request;
+use Faker\Generator as Faker;
 
 class EventManagerController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, Faker $faker)
     {
-        $jsonDataAsArray = $request->json()->all(); // This is how you would retrieve this with Laravel
-        $alexaRequest = \Alexa\Request\Request::fromData($jsonDataAsArray);
+        $alexaRequest = \Alexa\Request\Request::fromData($request->json()->all());
 
         $response = new \Alexa\Response\Response;
         if ($alexaRequest instanceof IntentRequest) {
-            $response->respond('Absicht erkannt: ' . $alexaRequest->intentName);
+            switch ($alexaRequest->intentName) {
+                case 'AddGuestIntent' :
+                    $responseText = 'Neuen Gast hinzufügen';
+
+                    break;
+
+                case 'RemoveGuestIntent' :
+                    $responseText = 'Gast entfernen';
+
+                    break;
+
+                case 'ConfirmGuestIntent' :
+                    $responseText = 'Gast bestätigen';
+
+                    break;
+
+                case 'CallOffGuestIntent' :
+                    $responseText = 'Gast absagen';
+
+                    break;
+
+                case 'GetGuestsListIntent' :
+                    $responseText = 'Gästeliste abfragen';
+
+                    break;
+
+                case 'GetConfirmedGuestsListIntent' :
+                    $responseText = 'Bestätigte Gästeliste abfragen';
+
+                    break;
+
+                case 'GetUnableGuestsListIntent' :
+                    $responseText = 'Abgesagte Gästeliste abfragen';
+
+                    break;
+
+                case 'GetUndecidedGuestsListIntent' :
+                    $responseText = 'Unbestätigte Gästeliste abfragen';
+
+                    break;
+
+                case 'GetGuestStatusIntent' :
+                    $responseText = 'Gaststatus abfragen';
+
+                    break;
+
+                case 'Intent' :
+                    $responseText = '';
+
+                    break;
+
+                default :
+                    $responses = [
+                        'Das habe ich leider nicht verstanden',
+                        'Das weiß ich leider nicht',
+                        'Es tut mir leid, damit kann ich dir nicht leider nicht helfen',
+                        'Es tut mir leid, das habe ich nicht verstanden',
+                        'Es tut mir leid, das weiß ich nicht',
+                        'Wie bitte?',
+                    ];
+                    $responseText = $faker->shuffleArray($responses);
+
+                    break;
+            }
+
+            $response->respond($responseText);
         } else {
-            $response->respond('Keine Absicht erkannt');
+            $responses = [
+                'Hallo',
+                'Herzlich Willkommen',
+                'Willkommen',
+            ];
+
+            $response->respond($faker->shuffleArray($responses));
         }
 
         return response()->json($response->render());
