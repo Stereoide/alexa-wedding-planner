c9c18914b703c481813002e51b42e25dc0992f23
Jörn Maske <j.maske@otterbach.de>
2018-01-03 11:18:54 +0100

    Switched from GuestControllerto EventMangerController

---
+++ app/Http/Controllers/EventManagerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Alexa\Request\IntentRequest;
+use Illuminate\Http\Request;
+
+class EventManagerController extends Controller
+{
+    public function index(Request $request)
+    {
+        $jsonDataAsArray = $request->json()->all(); // This is how you would retrieve this with Laravel
+        $alexaRequest = \Alexa\Request\Request::fromData($jsonDataAsArray);
+
+        $response = new \Alexa\Response\Response;
+        if ($alexaRequest instanceof IntentRequest) {
+            $response->respond('Absicht erkannt: ' . $alexaRequest->intentName);
+        } else {
+            $response->responde('Keine Absicht erkannt');
+        }
+
+        return response()->json($response->render());
+    }
+}
