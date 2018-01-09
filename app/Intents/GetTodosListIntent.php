42f02efd716f973bcadc92383dc652995c6947bd
Jörn Maske <j.maske@otterbach.de>
2018-01-09 23:31:35 +0100

    Added GetTodosListIntent

---
+++ app/Intents/GetTodosListIntent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Intents;
+
+use App\Todo;
+
+class GetTodosListIntent
+{
+    /* Methods */
+
+    public function process()
+    {
+        /* Fetch all guests */
+
+        $todosCompleted = Todo::forEvent($this->currentEvent->id)->completed()->get();
+        $todosOpen = Todo::forEvent($this->currentEvent->id)->open()->get();
+
+        if ($todosCompleted->isEmpty() && $todosOpen->isEmpty()) {
+            $responseText = 'Es liegen noch keine Aufgaben vor.';
+        } else {
+            $responseText = '';
+
+            if ($todosCompleted->isEmpty()) {
+                $responseText .= 'Es wurden noch keine Aufgaben erledigt';
+            } else {
+                if ($todosCompleted->count() == 1) {
+                    $responseText .= 'Eine Aufgabe wurde erledigt';
+                } else {
+                    $responseText .= $todosCompleted->count() . ' Aufgaben wurden erledigt';
+                }
+            }
+
+            $responseText .= ' und ';
+            if ($todosOpen->isEmpty()) {
+                $responseText .= 'es sind keine Aufgaben mehr offen.';
+            } else {
+                if ($todosOpen->count() == 1) {
+                    $responseText .= 'eine Aufgabe wurde noch nicht erledigt.';
+                } else {
+                    $responseText .= $todosOpen->count() . ' Aufgaben wurden noch nicht erledigt.';
+                }
+            }
+        }
+
+        return $responseText;
+    }
+}
