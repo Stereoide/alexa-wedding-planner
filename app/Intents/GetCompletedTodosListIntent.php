81c1dda3afa886ef7d41b9f50ebfd55fd97d90a3
Jörn Maske <j.maske@otterbach.de>
2018-01-09 23:30:55 +0100

    Added GetCompletedTodosListIntent

---
+++ app/Intents/GetCompletedTodosListIntent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Intents;
+
+use App\Todo;
+
+class GetCompletedTodosListIntent
+{
+    /* Methods */
+
+    public function process()
+    {
+        $todos = Todo::forEvent($this->currentEvent->id)->completed()->get();
+
+        if ($todos->isEmpty()) {
+            return 'Es wurden noch keine Aufgaben erledigt.';
+        } else {
+            $firstTodoNames = $todos->pluck('todo')->sort();
+            $lastTodoName = $firstTodoNames->splice($firstTodoNames->count() - 1)->first();
+
+            if ($firstTodoNames->isEmpty()) {
+                $responseText = 'Bisher wurde nur ' . $lastTodoName . ' erledigt.';
+            } else {
+                $responseText = 'Folgende Aufgaben wurden bereits erledigt: ' . implode(', ', $firstTodoNames->all()) . ' und ' . $lastTodoName;
+            }
+
+            return $responseText;
+        }
+    }
+}
