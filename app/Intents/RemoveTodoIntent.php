ba7853a2c968f0cabfaa4fbb30cece3053cfae08
Jörn Maske <j.maske@otterbach.de>
2018-01-09 23:32:51 +0100

    Added RemoveTodoIntent

---
+++ app/Intents/RemoveTodoIntent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Intents;
+
+use App\Todo;
+
+class RemoveTodoIntent
+{
+    /* Required slot values */
+
+    public $requiredSlots = ['Todo', ];
+
+    /* Methods */
+
+    public function process()
+    {
+        $todoName = $this->slots['Todo']->value;
+
+        Todo::forEvent($this->currentEvent->id)->where('todo', 'LIKE', $todoName)->delete();
+        return 'Ich habe ' . $todoName . ' von der Todo-Liste entfernt.';
+    }
+}
