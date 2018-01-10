b022e2d9bba6a1ac1c5c12834a03937bd348a916
Jörn Maske <j.maske@otterbach.de>
2018-01-10 14:58:33 +0100

    Improved Todo due date handling

--- app/Intents/GetRandomTodoIntent.php
+++ app/Intents/GetRandomTodoIntent.php
@@ -12,7 +12,13 @@ class GetRandomTodoIntent extends Intent
     {
         $todo = Todo::forEvent($this->currentEvent->id)->open()->inRandomOrder()->first();
         if (!is_null($todo)) {
-            return 'Zufällige offene Aufgabe: ' . $todo->todo;
+            $responseText = 'Zufällige offene Aufgabe: ' . $todo->todo;
+
+            if (!is_null($todo->due_at)) {
+                $responseText .= ', fällig am ' . $todo->due_at->formatLocalized('%A den %d. %B %Y');
+            }
+
+            return $responseText;
         } else {
             return 'Es gibt zur Zeit keine offenen Aufgaben.';
         }
